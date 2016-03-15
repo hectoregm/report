136dc6e66d98188eee45cabd1789912f2cb4c6fa
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-03-15 13:02:56 -0600

    Review up to Bezel::Config

--- code/bezel.rb
+++ code/bezel.rb
@@ -15,20 +15,7 @@ module Bezel
     end
 
     def context(tenant, tag, auth_token = nil)
-      o_tenant = Bezel.client.tenant
-      o_tag = Bezel.client.tag
-      o_auth_token = Bezel.client.auth_token
-      Bezel.client.tenant = tenant
-      Bezel.client.tag = tag
-      Bezel.client.auth_token = auth_token
-      yield
-    ensure
-      Bezel.client.tenant = o_tenant
-      Bezel.client.tag = o_tag
-      Bezel.client.auth_token = o_auth_token
+      # ...
     end
   end
-
-  # ...
-  
 end
