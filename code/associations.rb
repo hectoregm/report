37cb3d34cb17f0a4fc75a8b1337a4fac34ade23d
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-03-17 14:41:32 -0600

    Working in Associations module section.

--- code/associations.rb
+++ code/associations.rb
@@ -4,46 +4,42 @@ module Bezel::Associations
   end
 
   def has_many(field_name, model_name)
-    model = model_name.to_s.constantize
-    associations[field_name] = model
-    assoc_name = ActiveSupport::Inflector.pluralize(model.model_name.downcase)
-    define_assoc_method(field_name, assoc_name, model, false)
-  end
-
-  def belongs_to(field_name, model_name)
-    has_one(field_name, model_name)
+    model = get_model_class(model_name)
+    define_assoc_method(field_name, model, false)
   end
 
   def has_one(field_name, model_name)
-    model = model_name.to_s.constantize
-    associations[field_name] = model
-    assoc_name = model.model_name.downcase
-    define_assoc_method(field_name, assoc_name, model)
+    model = get_model_class(model_name)
+    define_assoc_method(field_name, model)
   end
 
   private
+  
+  def get_model_class(model_name)
+    model = model_name.to_s.constantize
+    associations[field_name] = model
+    model
+  end
 
-  def define_assoc_method(field_name, assoc_name, model, one = true)
+  def define_assoc_method(field_name, model, one = true)
     define_method(field_name) do
       value = @attributes[field_name.to_s] || @attributes[field_name.to_sym]
 
+      # Caundo el modelo tiene ya tiene los identificadores de los objetos asociados
       if value
         if one
           model.find(value["id"])
         else
           value.map {|v| model.find(v["id"]) }
         end
-      else
+      else # Se tienen que utilizar un JOIN para obtener los objetos asociados
         if id = @attributes["#{field_name}_id"]
           res = model.find(id)
         else
-          res = model.find(:all, filter: "#{c3_type.to_s.camelize(:lower)}.id == '#{@attributes["id"]}'")
+          res = model.find(:all, 
+            filter: "#{c3_type.to_s.camelize(:lower)}.id == '#{@attributes["id"]}'")
 
-          if one
-            res[0]
-          else
-            res
-          end
+          one ? res[0] : res
         end
       end
     end
