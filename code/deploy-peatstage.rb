d94e3e8fbda3cda403c21d983d303c3f23bfb5c0
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-04-07 14:15:55 -0500

    Added from capistrano tasks.

--- code/deploy-peatstage.rb
+++ code/deploy-peatstage.rb
@@ -2,45 +2,39 @@ set :hostname, 'dev-peatstage-ruby-01.c3-e.com'
 server "#{hostname}", :app, :web, :db, :primary => true
 
 set :branch, "#{ENV['branch'] || 'master'}"
-set :rails_env, "staging"
 set :application, "pge"
-set :deploy_to, "/home/#{user}/rails_app/peat-pge"
+set :deploy_to, "/home/nginx/rails_app/peat-pge"
 
-# Config overrides
 set :appdirname, "peat-pge"
 set :c3server, "http://peatstage.c3server.com:8080"
-
-before "deploy:restart", "deploy:upstart"
+set :rails_env, "staging"
 
 def override_defaults
   config_defaults[:appdirname] = appdirname
   config_defaults[:server_name] = server_name
   config_defaults[:c3server] = c3server
+  config_defaults[:railsenv] = rails_env
+end
+
+def write_and_transfer_config_files
+  override_defaults
+  ConfigCreator.new(config_defaults).write
+  puts "Transfering config/peat.yml"
+  transfer(:up, "tmp/peat.yml", "#{shared_path}/peat.yml")
+  puts "Transfering nginx conf file"
+  transfer(:up, "tmp/nginx.conf", "#{shared_path}/nginx.conf")
+  run "cp #{shared_path}/nginx.conf /opt/nginx/conf/nginx.conf.#{application}"
 end
 
 namespace :deploy do
-  desc "Peat adhoc setup"
   task :peat_setup do
-    override_defaults
-    ConfigCreator.new(config_defaults).write
-    puts "Transfering config/peat.yml"
-    transfer(:up, "tmp/peat.yml", "#{shared_path}/peat.yml")
-    puts "Transfering nginx conf file"
-    transfer(:up, "tmp/nginx.conf", "#{shared_path}/nginx.conf")
-    run "mkdir -p #{shared_path}/pdf_downloads"
+    write_and_transfer_config_files
   end
 end
 
 namespace :peat do
-  desc "Upload upto date peat.yml and nginx.conf, then it restarts the application."
   task :config_sync do
-    override_defaults
-    ConfigCreator.new(config_defaults).write
-    puts "Transfering config/peat.yml"
-    transfer(:up, "tmp/peat.yml", "#{shared_path}/peat.yml")
-    puts "Transfering nginx conf file"
-    transfer(:up, "tmp/nginx.conf", "#{shared_path}/nginx.conf")
-    run "cp #{shared_path}/nginx.conf /opt/nginx/conf/nginx.conf.#{application}"
+    write_and_transfer_config_files
   end
 end
 
---
+++ code/deploy-vars.rb
@@ -0,0 +1,24 @@
+set :config_defaults, {
+  appdirname: "peat",
+  c3server: "http://c3server.com:8080",
+  nginx_port: 8888,
+  rails_env: "production",
+  tenant: "peat",
+  tag: "prod",
+  # ...
+}
+
+set :stages, %w{peatstage peattest peatqa peatprod}
+set :application, "peat"
+
+set :scm, :git
+set :repository,  "git@server.com:c3energy/peat.git"
+set :branch, "#{ENV['branch'] || 'release/v1.0'}"
+
+# Tareas para inicializar el ambiente
+after "deploy:setup", "deploy:peat_setup"
+before 'deploy:setup', 'rvm:install_rvm'
+before 'deploy:setup', 'rvm:install_ruby'
+
+# Se reinicia el sistema despues de actualizar su configuración
+after "peat:config_sync", "deploy:restart"
