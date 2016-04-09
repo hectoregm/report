5ba43ab7439804469a205d59e21e5112f47cdda8
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-04-09 13:20:42 -0500

    RSpec code example

---
+++ code/rspec-questions-controller.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe QuestionsController do
+  before :each do
+    @building = mock_model(Building, id: "building_1",
+                           placed_at_account_id: 1,
+                           service_location_account_id: 1,
+                           recommendations_valid: true,
+                           locations: [])
+    Building.stub(:find) { @building }
+  end
+
+  describe "GET 'next'" do
+    before :each do
+      @bpa = mock_model(BuildingProfileAnswer, id: "bpa_1", to_partial_path: "questions/text")
+      PEATEngine.stub(:next_question) { @bpa }
+    end
+
+    it "gets the next question" do
+      PEATEngine.should_receive(:next_question).and_return(@bpa)
+      get :next, { building_id: "building_id"}
+    end
+  end
+end
